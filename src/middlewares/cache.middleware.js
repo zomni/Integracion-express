10b55bacb9ba3a09735a5ce0982360402966a589
Paolo <paol.vilches@duocuc.cl>
2025-05-05 01:20:35 -0400

    Tiempo de cache por rutas

--- src/middlewares/cache.middleware.js
+++ src/middlewares/cache.middleware.js
@@ -1,8 +1,24 @@
+// src/middlewares/cache.middleware.js
 const NodeCache = require('node-cache');
-const cache = new NodeCache({ stdTTL: 60 }); // 60 segundos
+const cache = new NodeCache();
+
+const rutaTTL = {
+  '/java-api/usuario/ListaUsuarios': 60,
+  '/java-api/producto': 120,
+  '/java-api/categoria': 180,
+  '/java-api/marca': 180,
+  '/java-api/sucursal': 300,
+  '/java-api/region': 300,
+  '/java-api/ciudad': 300,
+  '/java-api/comuna': 300,
+  '/java-api/tipoentrega': 300,
+  '/java-api/metodopago': 300,
+  '/java-api/rol': 300
+};
 
 const cacheMiddleware = (req, res, next) => {
   const key = req.originalUrl;
+  const ttl = rutaTTL[key] || 60; // TTL por defecto: 60 segundos
 
   const cachedResponse = cache.get(key);
   if (cachedResponse) {
@@ -12,10 +28,9 @@ const cacheMiddleware = (req, res, next) => {
     });
   }
 
-  // Captura la respuesta original
   const originalJson = res.json.bind(res);
   res.json = (body) => {
-    cache.set(key, body);
+    cache.set(key, body, ttl); // aplicar TTL personalizado
     return originalJson({
       cache: false,
       data: body
